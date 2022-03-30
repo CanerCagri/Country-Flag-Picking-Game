0ec257642f12e25c56dca3e16753808965eca5c9
CanerCagri <canerrcagrii@gmail.com>
2022-03-31 00:12:55 +0300

    Bug fix and ui update

--- Project2/CountryFlagPickingGame/ViewController.swift
+++ Project2/CountryFlagPickingGame/ViewController.swift
@@ -17,7 +17,8 @@ class ViewController: UIViewController {
     var correctAnswer = 0
     var score = 0
     var previousScores = [Scores]()
-    var askedQuestionCount = 0
+    var askedQuestionCount = -1
+    var maxQuestion = 11
     
     // All of the 3 challenges added to game!
     
@@ -49,32 +50,40 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
-        title! += "  Score : \(score)"
+        title! += "  Score : \(score)    \(askedQuestionCount)/ \(maxQuestion) "
     }
     
     @IBAction func buttonTaped(_ sender: UIButton) {
-        var title : String
+        var title2 : String
         
         if sender.tag == correctAnswer {
-            title = "Correct"
+            title2 = "Correct"
             score += 1
+            
+            title = countries[correctAnswer].uppercased()
+            title! += "  Score : \(score)    \(askedQuestionCount)/ \(maxQuestion) "
         } else{
-            title = "Wrong. That's the flag of : \(countries[sender.tag].uppercased())"
+            title2 = "Wrong. That's the flag of : \(countries[sender.tag].uppercased())"
             score -= 1
+            
+            title = countries[correctAnswer].uppercased()
+            title! += "  Score : \(score)    \(askedQuestionCount)/ \(maxQuestion) "
         }
-        var ac = UIAlertController(title: title, message: "", preferredStyle: .alert)
+        var ac = UIAlertController(title: title2, message: "", preferredStyle: .alert)
         
         if (askedQuestionCount < 10) {
             ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
         }else if (askedQuestionCount == 10){
-            title = "FINAL QUESTION"
-            ac.addAction(UIAlertAction(title: title, style: .default, handler: askQuestion))
+            title2 = "FINAL QUESTION"
+            ac.addAction(UIAlertAction(title: title2, style: .default, handler: askQuestion))
         }else {
             let lastScore = Scores(Score: score)
             previousScores.append(lastScore)
             save()
-            title = "GAME IS OVER!"
-            ac = UIAlertController(title: title, message: "FINAL SCORE : \(score)", preferredStyle: .alert)
+            title2 = "GAME IS OVER!"
+            
+            
+            ac = UIAlertController(title: title2, message: "FINAL SCORE : \(score)", preferredStyle: .alert)
             for (i,i2) in previousScores.enumerated() {
                 if score > i2.Score && i + 2 == previousScores.count{
                     let highScore = UIAlertAction(title: "HIGHEST SCORE IN GAME !!! CONGRATS", style: .default)
@@ -86,7 +95,7 @@ class ViewController: UIViewController {
             ac.addAction(UIAlertAction(title: "Play Again", style: .default) { [weak self] _ in
                 
                 self?.score = 0
-                self?.askedQuestionCount = 0
+                self?.askedQuestionCount = -1
                 self?.askQuestion()
             })
             loadSavedData()
